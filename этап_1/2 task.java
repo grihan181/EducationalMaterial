d00ce2dd90ac5c966c8096b908d30804c1d826a4
grihan181 <52749469+grihan181@users.noreply.github.com>
2022-02-10 20:08:23 +0400

    2 task

---
+++ этап_1/2 task.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        float c = 7.7f;
+        float d = 10.5f;
+        int result = a * b / a - b;
+        float result1 = c + d;
+        System.out.println(result);
+        System.out.println(result1);
+    }
+}
